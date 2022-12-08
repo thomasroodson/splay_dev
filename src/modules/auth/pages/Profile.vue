2b293a3be2057532db13739e5d9d741e05deda0c
thomasroodson <thomas.roodson@live.com>
2022-12-08 18:00:22 -0300

    new component

--- src/modules/auth/pages/Profile.vue
+++ src/modules/auth/pages/Profile.vue
@@ -2,14 +2,14 @@
     <section class="profile-area  mt-5 py-5">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-md-8 title-profile">
+          <div class="col-lg-8 title-profile">
             <h1>Conta <span>&</span> Perfil</h1>
           </div>
         </div>
       </div>
       <div class="container text-center">
         <div class="row justify-content-center">
-          <div class="col-md-8 d-flex py-4">
+          <div class="col-lg-8 d-flex py-4">
             <div class="avatar-profile d-flex align-items-center justify-content-center">
               <font-awesome-icon :icon="['fas', 'user']" size="2x"/>
             </div>
@@ -23,19 +23,19 @@
           </div>
         </div>
         <div class="row justify-content-center plans">
-          <div class="col-md-8 d-flex py-2">
-            <div class="col-md-4 d-flex align-items-center">
+          <div class="col-lg-8 d-flex py-2">
+            <div class="col-4 d-flex align-items-center">
               <h2 class="h4 m-0">Detalhes do Plano</h2>
             </div>
-            <div class="col-md-4 d-flex align-items-center">
+            <div class="col-3 d-flex align-items-center">
               {{ user.plano.titulo }}
             </div>
-            <div class="col-md-4 d-flex align-items-center">
+            <div class="col-5 d-flex align-items-center">
               <p class="m-0">{{ user.plano.descricao }}</p>
             </div>
           </div>
         </div>
-        <a class="btn btn-outline-light my-3 w-50" :href="`/${user.id}/profile/edit`">Atualizar dados</a>
+        <router-link class="btn btn-outline-light my-3 w-50" :to="{ name: 'profile-edit' }">Atualizar dados</router-link>
       </div>
     </section>
 </template>
@@ -46,13 +46,18 @@ export default {
   name: 'ProfilePage',
   computed: {
     ...mapState('auth', ['user'])
+  },
+  created () {
+    if (this.user) {
+      console.log('hello')
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .profile-area{
-  .col-md-8{
+  .col-lg-8{
     border-bottom: 1px solid #ccc;
   }
   .title-profile h1{
