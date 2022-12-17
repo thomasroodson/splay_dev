2d62cb62b69440a939bbafa5b72b88e11bed79c5
Thomas Roodson <thomas.roodson@gmail.com>
2022-12-17 19:17:56 -0300

    Old Code

--- src/components/player/Player.vue
+++ src/components/player/Player.vue
@@ -1,41 +1,13 @@
 <template>
-  <vue-plyr ref="plyr" :options="playerOptions">
-    <video :poster="post.media.cover" :id="'video-' + post.id" data-plyr-config="{'autoplay': false}">
-    </video>
-  </vue-plyr>
+  <div></div>
 </template>
 
 <script>
-import VuePlyr from 'vue-plyr'
-import Hls from 'hls.js'
-
 export default {
-  name: 'ComponentName',
-  components: {
-    VuePlyr
-  },
-  data () {
-    return {
-      playerOptions: {
-        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
-        settings: ['quality', 'speed', 'loop']
-      }
-    }
-  },
-  computed: {
-    player () {
-      console.log(this.$refs.plyr.player)
-      return this.$refs.plyr.player
-    }
-  },
-  mounted () {
-    if (Hls.isSupported()) {
-      const hls = new Hls()
-      hls.loadSource('https://devimages.apple.com.edgekey.net/iphone/samples/bipbop/bipbopall.m3u8')
-      hls.attachMedia(this.player.media)
-
-      window.hls = hls
-    }
-  }
+  name: 'PlayerComponent'
 }
 </script>
+
+<style>
+
+</style>
