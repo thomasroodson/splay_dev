439bf32d762374810eba696979193d750be47a82
thomasroodson <thomas.roodson@live.com>
2022-12-19 18:32:35 -0300

    method payment

--- src/main.js
+++ src/main.js
@@ -10,12 +10,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 // import Vuelidate from 'vuelidate'
-import VueVideoPlayer from 'vue-video-player'
-import 'video.js/dist/video-js.css'
-import 'vue-video-player/src/custom-theme.css'
-import 'videojs-flash'
-import 'videojs-contrib-hls/dist/videojs-contrib-hls'
-// import 'videojs-contrib-hls.js/src/videojs.hlsjs'
 
 library.add(fas, fab)
 
@@ -23,7 +17,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Vue.use(Vuelidate)
 Vue.use(VuePlyr)
-Vue.use(VueVideoPlayer)
 
 Vue.config.productionTip = false
 
--- src/modules/auth/pages/ProfileEdit.vue
+++ src/modules/auth/pages/ProfileEdit.vue
@@ -1,92 +1,14 @@
 <template>
   <section id="profile-edit">
     <div class="container mt-5 py-5">
-      <div class="row justify-content-center">
-        <div class="col-lg-8 title-profile">
-          <h1>Editar Perfil</h1>
-        </div>
-        <div class="col-lg-8 no-border py-4">
-
-          <form @submit.prevent="ActionUpdateProfile(user)">
-            <div class="form-group">
-              <label>Nome Completo</label>
-              <input
-                type="text"
-                class="form-control"
-                v-model="user.nome"
-              >
-            </div>
-            <div class="d-flex flex-wrap">
-              <div class="form-group pe-2 col-md-6">
-                <label>Email</label>
-                <input type="email" class="form-control" v-model="user.email">
-              </div>
-              <div class="form-group col-md-6">
-                <label>CPF</label>
-                <input type="text" class="form-control" v-model="user.cpf" v-mask="'###.###.###-##'">
-              </div>
-            </div>
-            <div class="d-flex flex-wrap">
-              <div class="form-group col-md-6 pe-2">
-                <label>Data de Nascimento</label>
-                <input type="text" :value="user.nascimento.split('-').reverse()" class="form-control" v-mask="'##/##/####'">
-              </div>
-              <div class="form-group col-md-6">
-                <label>Celular</label>
-                <input type="text" class="form-control" v-model="user.celular" v-mask="'(##) #####-####'">
-              </div>
-            </div>
-            <div class="d-flex flex-wrap">
-              <div class="form-group col-md-5 pe-2">
-                <label>Endereço</label>
-                <input type="text" class="form-control" placeholder="Avenida Alameda das Travessas">
-              </div>
-              <div class="form-group col-md-2 pe-2">
-                <label>Número</label>
-                <input type="text" class="form-control" placeholder="102">
-              </div>
-              <div class="form-group col-md-5">
-                <label>Complemento</label>
-                <input type="text" class="form-control" placeholder="Apartamento, Casa">
-              </div>
-            </div>
-            <div class="d-flex flex-wrap">
-              <div class="form-group col-md-4 pe-2">
-                <label>CEP</label>
-                <input type="text" class="form-control" placeholder="Avenida Alameda das Travessas" v-mask="'##.###-###'">
-              </div>
-              <div class="form-group col-md-4 pe-2">
-                <label>Estado</label>
-                <input type="text" class="form-control" placeholder="102">
-              </div>
-              <div class="form-group col-md-4">
-                <label>Cidade</label>
-                <input type="text" class="form-control" placeholder="Apartamento, Casa">
-              </div>
-              <div class="form-group col-md-12">
-                <label>País</label>
-                <input type="text" class="form-control" placeholder="País">
-              </div>
-            </div>
-          </form>
-          <div class="d-flex mt-3">
-            <div class="flex-grow-1 pe-2">
-              <button type="submit" class="btn btn-dark w-100">Atualizar</button>
-            </div>
-            <div>
-              <button type="button" class="btn btn-secondary px-4">Pagamento</button>
-            </div>
-          </div>
-        </div>
-      </div>
+        <Payments v-if="true" />
     </div>
   </section>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
-import { required, minLength } from 'vuelidate/lib/validators'
-import { mask } from 'vue-the-mask'
+import Payments from '../components/payments/Payments'
 
 export default {
   name: 'ProfileEdit',
@@ -96,16 +18,8 @@ export default {
   methods: {
     ...mapActions('auth', ['ActionUpdateProfile'])
   },
-  directives: {
-    mask
-  },
-  validations: {
-    user: {
-      nome: {
-        required,
-        minLength: minLength(4)
-      }
-    }
+  components: {
+    Payments
   }
 }
 </script>
